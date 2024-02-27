53ccd0babf6bfec111847c6efe27c342795af40f
sohail <as19adj@herts.ac.uk>
2024-02-27 12:56:12 +0000

    adds starter code

---
+++ starter/lib/Employee.js
@@ -0,0 +1,32 @@
+// TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
+Employee.prototype.getName = function(){
+    return this.name;
+
+};
+Employee.prototype.getId = function(){
+    return this.id;
+
+};
+Employee.prototype.getEmail = function(){
+    return this.email
+};
+Employee.prototype.getRole = function(){
+    const text = `${this.name}
+    Id = ${this.getId()}
+    Email = ${this.getEmail()}
+    `
+    console.log(text);
+    console.log('======================');
+};
+
+const emp = new Employee('Sohail', '1', 'ss@hotmail.com');
+
+emp.getRole();
